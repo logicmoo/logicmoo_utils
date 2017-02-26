647e6165f074f6def16f89485f5306d86d843cc1
root <logicmoo@gmail.com>
2017-02-26 10:23:45 -0800

    v0.0.8

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(logicmoo_utils).
+version('0.0.8').
+title('Common predicates used by external Logicmoo Utils and Base').
+keywords([source]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
+maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
+home( 'https://github.com/TeamSPoon/logicmoo_utils' ).
+download( 'https://github.com/TeamSPoon/logicmoo_utils/release/*.zip' ).
+autoload(true).
