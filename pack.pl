e769cb0b7c362ea47e03e340174a6af189b620d1
root <logicmoo@gmail.com>
2017-03-16 20:54:52 -0700

    v1.1.114

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(logicmoo_utils).
-version('1.1.113').
+version('1.1.114').
 title('Common predicates used by external Logicmoo Utils and Base').
 keywords([source]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
