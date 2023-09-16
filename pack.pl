da60f64236fba4724b29664ace83b8a9e668a814
Douglas R Miles <logicmoo@gmail.com>
2023-09-16 01:56:37 -0700

    3.1.2

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(logicmoo_utils).
-version('3.0.3').
+version('3.1.3').
 title('Common predicates that are used throughout LogicMOO Software').
 keywords([local_scoping,utilities,debugging,loop_checking]).
 author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
